f3c642361f8d19b9224ba6f7e2fcc405ab6677e7
grollcake <grollcake@gmail.com>
2021-08-26 13:13:02 +0900

    3번째 학습

---
+++ main_app/lib/challenges/003_incoming_call_screen/003_incoming_call.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'components/circular_button.dart';
+
+class Challenge003 extends StatelessWidget {
+  const Challenge003({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Image.asset(
+          'assets/images/chanbyul001.jpg',
+          fit: BoxFit.cover,
+        ),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.3),
+          ),
+        ),
+        SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20),
+                Text(
+                  "김찬별",
+                  style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                SizedBox(height: 0),
+                Text(
+                  '일산의 자랑',
+                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Incoming 00:02'.toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white60,
+                  ),
+                ),
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CircularButton(icon: Icons.mic, bgColor: Colors.white),
+                    CircularButton(icon: Icons.call_end, bgColor: Colors.red),
+                    CircularButton(icon: Icons.headphones, bgColor: Colors.white),
+                  ],
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- main_app/lib/model/ui_challenges.dart
+++ main_app/lib/model/ui_challenges.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:main_app/challenges/001_calculator.dart';
+import 'package:main_app/challenges/002_calculator.dart';
+import 'package:main_app/challenges/003_incoming_call_screen/003_incoming_call.dart';
 
 class UIChallenge {
-  @required String title;
-  @required String description;
-  @required Widget screen;
+  @required
+  String title;
+  @required
+  String description;
+  @required
+  Widget screen;
 
-  UIChallenge({required this.title, required this.description, required this.screen}): super();
+  UIChallenge({required this.title, required this.description, required this.screen}) : super();
 }
 
 List<UIChallenge> uiChallenges = [
-  UIChallenge(title: 'calculator', description: 'Expanded의 활용', screen: Challenge001())
+  UIChallenge(title: 'Simple Calculator', description: 'Expanded의 활용', screen: Challenge001()),
+  UIChallenge(title: 'Colorful Calculator', description: 'GridView.builder', screen: Challenge002()),
+  UIChallenge(title: 'Incoming Call', description: 'Stack', screen: Challenge003()),
 ];
