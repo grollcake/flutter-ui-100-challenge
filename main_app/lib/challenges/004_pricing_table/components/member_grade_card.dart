d73faf6571fa0bbde2112d9885a5349c88fc6551
grollcake <grollcake@gmail.com>
2021-08-28 15:24:01 +0900

    위젯 분리 및 Spacer() 적용

--- main_app/lib/challenges/004_pricing_table/components/member_grade_card.dart
+++ main_app/lib/challenges/004_pricing_table/components/member_grade_card.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class MemberGradeCard extends StatelessWidget {
   const MemberGradeCard({
     Key? key,
-    required this.cardColor, required this.gradeLevel, required this.price, required this.desc,
+    required this.cardColor,
+    required this.gradeLevel,
+    required this.price,
+    required this.desc,
   }) : super(key: key);
 
-  final double smallBoxSize = 20.0;
   final Color cardColor;
   final String gradeLevel;
   final int price;
@@ -20,109 +22,148 @@ class MemberGradeCard extends StatelessWidget {
         padding: EdgeInsets.all(20),
         child: Column(
           children: [
-            Stack(
-              children: [
-                Container(
-                  height: 200,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20)), color: cardColor),
-                ),
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: Container(
-                    width: smallBoxSize,
-                    height: smallBoxSize,
-                    color: Colors.white.withOpacity(.8),
-                  ),
-                ),
-                Positioned(
-                  top: 0,
-                  right: smallBoxSize,
-                  child: Container(
-                    width: smallBoxSize,
-                    height: smallBoxSize,
-                    color: Colors.white.withOpacity(.4),
-                  ),
-                ),
-                Positioned(
-                  top: smallBoxSize,
-                  right: 0,
-                  child: Container(
-                    width: smallBoxSize,
-                    height: smallBoxSize,
-                    color: Colors.white.withOpacity(.4),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 40),
-                  child: Column(
-                    children: [
-                      Text(
-                        this.gradeLevel,
-                        style: TextStyle(fontSize: 14, color: Colors.white, letterSpacing: 2),
-                      ),
-                      SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.baseline,
-                        textBaseline: TextBaseline.alphabetic,
-                        children: [
-                          Text(
-                            r'$',
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white),
-                          ),
-                          SizedBox(width: 3),
-                          Text(
-                            this.price.toString(),
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        this.desc,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 14, color: Colors.white),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            Container(
-              height: 120,
-              width: double.infinity,
-              decoration: BoxDecoration(color: cardColor.withOpacity(.2)),
-              child: Column(
-                children: [
-                  SizedBox(height: 20),
-                  SizedBox(
-                    height: 50,
-                    width: 180,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Get started',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      style: TextButton.styleFrom(
-                        backgroundColor: cardColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            CardUpperSide(cardColor: cardColor, gradeLevel: gradeLevel, price: price, desc: desc),
+            CardLowerSide(cardColor: cardColor),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CardUpperSide extends StatelessWidget {
+  const CardUpperSide({
+    Key? key,
+    required this.cardColor,
+    required this.gradeLevel,
+    required this.price,
+    required this.desc,
+  }) : super(key: key);
+
+  final double decoBoxSize = 20;
+  final Color cardColor;
+  final String gradeLevel;
+  final int price;
+  final String desc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        renderBackground(),
+        renderContents(),
+      ],
+    );
+  }
+
+  Widget renderBackground() {
+    return Stack(
+      children: [
+        Container(
+          height: 200,
+          decoration: BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(20)), color: cardColor),
+        ),
+        _decoratedBox(top: 0, right: 0, opacity: 0.8),
+        _decoratedBox(top: 0, right: decoBoxSize, opacity: 0.4),
+        _decoratedBox(top: decoBoxSize, right: 0, opacity: 0.4),
+      ],
+    );
+  }
+
+  Positioned _decoratedBox({required double top, required double right, required double opacity}) {
+    final double _boxSize = 20.0;
+
+    return Positioned(
+      top: top,
+      right: right,
+      child: Container(
+        width: _boxSize,
+        height: _boxSize,
+        color: Colors.white.withOpacity(opacity),
+      ),
+    );
+  }
+
+  Container renderContents() {
+    return Container(
+      height: 200,
+      padding: EdgeInsets.only(top: 40, bottom: 20),
+      child: Column(
+        children: [
+          Text(
+            this.gradeLevel,
+            style: TextStyle(fontSize: 14, color: Colors.white, letterSpacing: 2),
+          ),
+          Spacer(flex: 1),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              Text(
+                r'$',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white),
+              ),
+              SizedBox(width: 3),
+              Text(
+                this.price.toString(),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40, color: Colors.white),
+              ),
+            ],
+          ),
+          Spacer(flex: 1),
+          Text(
+            this.desc,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 14, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CardLowerSide extends StatelessWidget {
+  const CardLowerSide({
+    Key? key,
+    required this.cardColor,
+  }) : super(key: key);
+
+  final Color cardColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      //width: double.infinity,
+      alignment:Alignment.topCenter,
+      decoration: BoxDecoration(color: cardColor.withOpacity(.2)),
+      child: Column(
+        children: [
+          Spacer(flex: 1),
+          SizedBox(
+            height: 50,
+            width: 180,
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                'Get started',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              style: TextButton.styleFrom(
+                backgroundColor: cardColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+          ),
+          Spacer(flex: 2),
+        ],
       ),
     );
   }
